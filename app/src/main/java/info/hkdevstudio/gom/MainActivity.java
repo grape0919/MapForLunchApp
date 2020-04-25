ab1f302f06a519fc8b5d83ab67457d5e6457616c
ghdry2563 <ghdry2563@gmail.com>
2020-04-25 17:45:47 +0900

    카카오맵 연동 MapView 구현

--- app/src/main/java/info/hkdevstudio/gom/MainActivity.java
+++ app/src/main/java/info/hkdevstudio/gom/MainActivity.java
@@ -2,6 +2,8 @@ package info.hkdevstudio.gom;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.ViewGroup;
+import net.daum.mf.map.api.MapView;
 
 public class MainActivity extends Activity {
     @Override
@@ -9,5 +11,9 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        MapView mapView = new MapView(this);
+
+        ViewGroup mapViewContainer = findViewById(R.id.map_view);
+        mapViewContainer.addView(mapView);
     }
 }
